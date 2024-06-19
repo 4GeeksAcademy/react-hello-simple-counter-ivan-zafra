b1b4dedc978b6966c2ac642d968dde762d074d4d
Ivanzr17 <ivanzafrarendal@gmail.com>
2024-06-19 21:21:35 +0200

    gracias manuel <3

--- src/js/components/contador.jsx
+++ src/js/components/contador.jsx
@@ -5,10 +5,29 @@ import { useEffect } from "react";
 function Counter() {
   const [counterState, setCounterState] = useState(0);
 
+  /*
   useEffect(() => {
     counterState >= 0 &&
-      setTimeout(() => setCounterState(counterState + 1), 1000);
-  }, [counterState]);
+      setInterval(
+        () => setCounterState((prevCounterState) => prevCounterState + 1),
+        1000
+      );
+  }, []);
+
+
+*/
+
+  useEffect(() => {
+    if (counterState >= 0) {
+      const setIntervalReference = setInterval(
+        () => setCounterState((prevCounterState) => prevCounterState + 1),
+        1000
+      );
+
+      // Cleanup function to clear the interval
+      return () => clearInterval(setIntervalReference);
+    }
+  }, []);
 
   return <div class="text-center mt-5 h1 ">{counterState}</div>;
 }
